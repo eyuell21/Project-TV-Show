217b7ab6f10859cf5d609bf2217f68321db9c66f
eyuell21 <abrahameyuel4@gmail.com>
2025-03-22 10:52:02 +0000

    Implemented a function to render the filmcards.

--- script.js
+++ script.js
@@ -4,9 +4,37 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+const filmCardContainer = document.getElementById("filmCard-container");
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const allEpisodes = getAllEpisodes();
+  allEpisodes.forEach(filmData =>{
+    const filmCard = document.createElement('div')
+    filmCard.classList.add("film-card")
+
+    const bannerImg = document.createElement('img');
+    bannerImg.src =filmData.image.medium
+    filmCard.appendChild(bannerImg);
+
+    const titleElement = document.createElement('h3');
+    titleElement.textContent = filmData.name;
+    filmCard.appendChild(titleElement);
+
+    const summaryElement = document.createElement('p');
+    summaryElement.textContent = filmData.summary.replace(/<[^>]*>/g, '')
+    filmCard.appendChild(summaryElement);
+
+    const linkElement = document.createElement('a');
+    linkElement.href = filmData.url;
+    linkElement.textContent = "Re-direct to www.tvmaze.com"
+    linkElement.classList.add('redirect')
+    filmCard.appendChild(linkElement);
+
+    filmCardContainer.appendChild(filmCard)
+
+
+  })
+  
 }
 
 window.onload = setup;
